01ebf033808dd51d79f59cec7b201aad1c0c9106
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-01-29 14:15:20 +0200

    added DOM events-lab

---
+++ DOM Events-Lab/04. Mouse-Gradient/app.js
@@ -0,0 +1,13 @@
+function attachGradientEvents() {
+    const gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onMove);
+    const result = document.getElementById('result');
+
+    function onMove(ev) {
+        const box = ev.target;
+        const offset = Math.floor(ev.offsetX / box.offsetWidth * 100);
+        // za judge => Math.floor(ev.offsetX / box.clientWidth * 100)
+
+        result.textContent = `${offset}%`;
+    }
+}
