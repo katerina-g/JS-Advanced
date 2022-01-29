b894db3477123dc952efb0419ee5d81178cb3140
katerina-g <71485428+katerina-g@users.noreply.github.com>
2022-01-29 14:02:37 +0200

    added DOM introduction-lab

---
+++ DOM Introduction-Lab/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,6 @@
+function colorize() {
+    let rows = document.querySelectorAll('table tr:nth-child(even)');
+    for (let i = 0; i < rows.length; i ++) {
+        rows[i].style.backgroundColor = 'teal';
+    }
+}
